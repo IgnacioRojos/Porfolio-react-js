52a883049ae501d10b5c7cc36a7c6429c55c75d4
IgnacioRojos <nachorojos99@gmail.com>
2025-07-29 21:26:53 -0300

    se agrego un autoscroll

--- src/Componentes/Skills/skills.jsx
+++ src/Componentes/Skills/skills.jsx
@@ -1,42 +1,54 @@
 import "./skills.css";
 import Image from 'react-bootstrap/Image';
-import logo1 from "./css.jpg"
-import logo2 from "./html.jpg"
-import logo3 from "./github.png"
-import logo4 from "./JavaScript.jpg"
-import logo5 from "./nodejs.png"
-import logo6 from "./React.png"
-import logo7 from "./mongodb.png"
-import logo8 from "./handlebars.png"
-import logo9 from "./express-js.png"
+import { useEffect, useRef } from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+import logo1 from "./css.jpg";
+import logo2 from "./html.jpg";
+import logo3 from "./github.png";
+import logo4 from "./JavaScript.jpg";
+import logo5 from "./nodejs.png";
+import logo6 from "./React.png";
+import logo7 from "./mongodb.png";
+import logo8 from "./handlebars.png";
+import logo9 from "./express-js.png";
 
 const Skills = () => {
+  const logos = [logo1, logo2, logo3, logo4, logo5, logo6, logo7, logo8, logo9];
+
   return (
     <>
       <div className="col-lg-12 col-12 skills-header">
         <h2 className="skills-title">Habilidades</h2>
       </div>
 
-      <div className="row row-cols-1 row-cols-md-3 g-4 skills-grid">
-        <Image className="skills-img" src={logo1} alt="CSS" />
-        <Image className="skills-img" src={logo2} alt="HTML" />
-        <Image className="skills-img" src={logo3} alt="GitHub" />
+      <div className="skills-carousel-container">
+        <Splide
+          options={{
+            type: 'loop',
+            drag: 'free',
+            focus: 'center',
+            perPage: 3,
+            autoScroll: {
+              speed: 1,
+            },
+            arrows: false,
+            pagination: false,
+            gap: '0',
+          }}
+          extensions={{ AutoScroll }}
+        >
+          {logos.map((logo, index) => (
+            <SplideSlide key={index}>
+              <Image src={logo} alt={`Skill ${index}`} className="skills-img" />
+            </SplideSlide>
+          ))}
+        </Splide>
       </div>
 
-      <div className="row row-cols-1 row-cols-md-3 g-4 skills-grid">
-        <Image className="skills-img" src={logo4} alt="JavaScript" />
-        <Image className="skills-img" src={logo5} alt="Node.js" />
-        <Image className="skills-img" src={logo6}alt="React" />
-      </div>
-
-      <div className="row row-cols-1 row-cols-md-3 g-4 skills-grid">
-        <Image className="skills-img" src={logo7} alt="MongoDB" />
-        <Image className="skills-img" src={logo8}alt="Handlebars" />
-        <Image className="skills-img" src={logo9} alt="Express.js" />
-      </div>
-
-      <div className="col skills-text">
-        Además de las habilidades mencionadas en el área de programación, mis superiores destacaron mi predisposición para trabajar en equipo
+      <div className="col skills-text mt-4">
+        Además de las habilidades mencionadas en el área de programación, mis superiores destacaron mi predisposición para trabajar en equipo,
         dando mi opinión sobre decisiones o temas a mejorar en el rendimiento del grupo. Dentro de mi equipo de trabajo me felicitaron por 
         tomar el liderazgo en momentos donde hay que tomar decisiones bajo presión y la ayuda que ofrezco a los demás miembros en situaciones
         problemáticas que se presentan en la jornada laboral. Algo que también destaco es que en todo momento estoy dispuesto a recibir un 
@@ -47,8 +59,6 @@ const Skills = () => {
 };
 
 export default Skills;
-
-
 
 
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./App.css"
 import reportWebVitals from './reportWebVitals';
+import '@splidejs/react-splide/css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
